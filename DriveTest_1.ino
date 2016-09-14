be50c7127724377fba82d6b816a553ea0f3074dc
robertsbarr <RobertBarr@152.233.21.198.tigernet.wifi.dyn.clemson.edu>
2016-09-14 08:05:01 -0400

    testing

---
+++ DriveTest_1.ino
@@ -0,0 +1,37 @@
+
+
+// declaring valiables
+int drive_pin_right = 9;
+int drive_pin_left = 10;
+#define DRIVE 255;
+#define HALT 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(drive_pin_left, OUTPUT);
+  pinMode(drive_pin_right, OUTPUT);
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+    //turning the pins on
+    analogWrite(drive_pin_right, DRIVE);
+    analogWrite(drive_pin_left, DRIVE);
+
+    // waiting 2 seconds before turning off
+    delay(2000);
+
+    // turning pins off
+    analogWrite(drive_pin_right, HALT);
+    analogWrite(drive_pin_left, HALT);
+
+
+    // waiting 2 seconds before looping again
+    delay(2000);
+
+
+}
